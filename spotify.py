69a942ffb57ab9f48bd70c664570a23287ee0f01
Solomon Himelbloom <2001solomon@gmail.com>
2020-05-29 13:02:07 -0800

    Add automation script file

---
+++ spotify.py
@@ -0,0 +1,10 @@
+# Alaska Center ICE
+# Students2Startups
+# 05/29/2020
+#
+# Remo + Slack Music Bot
+
+import random
+
+# Windows/macOS Automation Script
+print("Remo + Slack Application Template")
